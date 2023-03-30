5cfe0c002cc664cab840c2ed638a6d08a1175484
OLAKU TEMITOPE <olakutope4@gmail.com>
2023-03-30 01:10:05 +0100

    Submission in 1 file

--- main.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('form');
-const thankYouMessage = document.querySelector('#submission');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-submission.classList.add('show');
-// setTimeout(() => form.submit(), 2000);
-});
